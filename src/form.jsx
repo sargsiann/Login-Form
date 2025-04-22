4b7d894579789f01add6c7c4b2bd1807267321cd
sargsyandavid7 <sargsyand648@gmail.com>
2025-04-22 20:24:05 +0400

    feat: Add password and email validation

--- src/form.jsx
+++ src/form.jsx
@@ -21,19 +21,34 @@ function Form(props) {
   }
 
 
+  const isValidEmail = (email) => {
+	const re = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+	return re.test(email);
+  };
+
+  const isValidPassword = (password) => {
+	const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
+	return re.test(password);
+  }
+
+
+
+
   return (
-  <div class="login-container">
+  <div className="login-container">
     <h2>Login</h2>
     <form action="#" method="POST">
-      <div class="form-group">
-        <label for="email">Email</label>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
         <input type="email" id="email" onChange={(e) => {handleInfoChange(e,"email")}} name="email" value={info.email} placeholder="you@example.com" required />
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" onChange={(e) => {handleInfoChange(e,"password")}} id="password" name="password" value={info.password} placeholder="••••••••" required />
-      </div>
-      <button type="submit" class="submit-btn">Sign In</button>
+		{!isValidEmail(info.email) && <div className="error" id="email-error">Invalid email address</div>}
+	</div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+      	<input type="password" onChange={(e) => {handleInfoChange(e,"password")}} id="password" name="password" value={info.password} placeholder="••••••••" required />
+		{!isValidPassword(info.password) && <div className="error" id="password-error">Incorrect password</div>}
+	  </div>
+	  <button type="submit" className="submit-btn" disabled = {!(isValidPassword(info.password) && isValidEmail(info.email))}>Sign In</button>
     </form>
   </div>)
 }
